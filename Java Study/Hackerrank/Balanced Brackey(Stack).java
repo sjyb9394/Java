105adb8e51d356ca20fe226ac0228b2a310059a7
sjyb9394 <69370122+sjyb9394@users.noreply.github.com>
2021-09-01 23:27:06 -0400

    Create Balanced Brackey(Stack).java

---
+++ Java Study/Hackerrank/Balanced Brackey(Stack).java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'isBalanced' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String isBalanced(String s) {
+    // Write your code here
+        int len=s.length();
+        if(len==0 || s==null) return "YES";
+        Stack<Character> stack = new Stack<Character>();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{')  stack.push(s.charAt(i));
+            else if(s.charAt(i)==')' && !stack.empty() && stack.peek()=='(') stack.pop();
+            else if(s.charAt(i)==']' && !stack.empty() && stack.peek()=='[') stack.pop();
+            else if(s.charAt(i)=='}' && !stack.empty() && stack.peek()=='{') stack.pop();
+            else return "NO";
+
+
+    }
+    if(stack.empty()) return "YES";
+    else return "NO";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                String result = Result.isBalanced(s);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
